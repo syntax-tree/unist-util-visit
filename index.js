d1fd42ecbbd70594865ec1199ab6ad91a733aa73
Titus Wormer <tituswormer@gmail.com>
2016-03-25 12:42:58 +0100

    Refactor interal interface

--- index.js
+++ index.js
@@ -1,99 +1,75 @@
 /**
  * @author Titus Wormer
- * @copyright 2015 Titus Wormer. All rights reserved.
+ * @copyright 2015 Titus Wormer
+ * @license MIT
  * @module unist:util:visit
  * @fileoverview Recursively walk over unist nodes.
  */
 
 'use strict';
 
-/**
- * Walk forwards.
- *
- * @param {Array.<*>} values - Things to iterate over,
- *   forwards.
- * @param {function(*, number): boolean} callback - Function
- *   to invoke.
- * @return {boolean} - False if iteration stopped.
- */
-function forwards(values, callback) {
-    var index = -1;
-    var length = values.length;
-
-    while (++index < length) {
-        if (callback(values[index], index) === false) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-/**
- * Walk backwards.
- *
- * @param {Array.<*>} values - Things to iterate over,
- *   backwards.
- * @param {function(*, number): boolean} callback - Function
- *   to invoke.
- * @return {boolean} - False if iteration stopped.
- */
-function backwards(values, callback) {
-    var index = values.length;
-    var length = -1;
-
-    while (--index > length) {
-        if (callback(values[index], index) === false) {
-            return false;
-        }
-    }
-
-    return true;
-}
+/* eslint-env commonjs */
 
 /**
  * Visit.
  *
  * @param {Node} tree - Root node
  * @param {string} [type] - Node type.
- * @param {function(node): boolean?} callback - Invoked
+ * @param {function(node): boolean?} visitor - Invoked
  *   with each found node.  Can return `false` to stop.
  * @param {boolean} [reverse] - By default, `visit` will
  *   walk forwards, when `reverse` is `true`, `visit`
  *   walks backwards.
  */
-function visit(tree, type, callback, reverse) {
-    var iterate;
-    var one;
-    var all;
-
+function visit(tree, type, visitor, reverse) {
     if (typeof type === 'function') {
-        reverse = callback;
-        callback = type;
+        reverse = visitor;
+        visitor = type;
         type = null;
     }
 
-    iterate = reverse ? backwards : forwards;
-
     /**
-     * Visit `children` in `parent`.
+     * Visit children in `parent`.
+     *
+     * @param {Array.<Node>} children - Children of `node`.
+     * @param {Node?} parent - Parent of `node`.
+     * @return {boolean?} - `false` if the visiting stopped.
      */
-    all = function (children, parent) {
-        return iterate(children, function (child, index) {
-            return child && one(child, index, parent);
-        });
-    };
+    function all(children, parent) {
+        var step = reverse ? -1 : 1;
+        var max = children.length;
+        var min = -1;
+        var index = (reverse ? max : min) + step;
+        var child;
+
+        while (index > min && index < max) {
+            child = children[index];
+
+            if (child && one(child, index, parent) === false) {
+                return false;
+            }
+
+            index += step;
+        }
+
+        return true;
+    }
 
     /**
      * Visit a single node.
+     *
+     * @param {Node} node - Node to visit.
+     * @param {number?} [index] - Position of `node` in `parent`.
+     * @param {Node?} [parent] - Parent of `node`.
+     * @return {boolean?} - A result of invoking `visitor`.
      */
-    one = function (node, index, parent) {
+    function one(node, index, parent) {
         var result;
 
         index = index || (parent ? 0 : null);
 
         if (!type || node.type === type) {
-            result = callback(node, index, parent || null);
+            result = visitor(node, index, parent || null);
         }
 
         if (node.children && result !== false) {
@@ -101,7 +77,7 @@ function visit(tree, type, callback, reverse) {
         }
 
         return result;
-    };
+    }
 
     one(tree);
 }
