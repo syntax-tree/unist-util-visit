806a79b8840b39e51d8ba87160f17bee59bd3166
Christian Murphy <Christian.Murphy.42@gmail.com>
2019-07-20 21:40:50 -0700

    types: add typings and type tests for unist-util-visit

---
+++ types/index.d.ts
@@ -0,0 +1,94 @@
+// TypeScript Version: 3.5
+
+import {Node, Parent} from 'unist'
+
+interface Visitor<V extends Node> {
+  (node: V, index: number, parent: Node): void
+}
+
+/**
+ * Check that type property matches expectation for a node
+ *
+ * @typeParam T type of node that passes test
+ */
+type TestType<T extends Node> = T['type']
+
+/**
+ * Check that some attributes on a node are matched
+ *
+ * @typeParam T type of node that passes test
+ */
+type TestObject<T extends Node> = Partial<T>
+
+/**
+ * Check if a node passes a test
+ *
+ * @typeParam T type of node that passes test
+ */
+interface TestFunction<T extends Node> {
+  /**
+   * Check if a node passes a test
+   *
+   * @param node node to check
+   * @param index index of node in parent
+   * @param parent parent of node
+   */
+  (node: Node, index?: number, parent?: Parent): node is T
+}
+
+/**
+ * Union of all the types of tests
+ *
+ * @typeParam T type of node that passes test
+ */
+type Test<T extends Node> = TestType<T> | TestObject<T> | TestFunction<T>
+
+/**
+ * Visit children of tree which pass a test
+ *
+ * @param tree abstract syntax tree to visit
+ * @param test test node
+ * @param visitor function to run for each node
+ * @param reverse visit the tree in reverse, defaults to false
+ * @typeParam T tree node
+ * @typeParam V node type to visit
+ */
+declare function visit<T extends Node, V extends Node>(
+  tree: T,
+  test: Test<V>,
+  visitor: Visitor<V>,
+  reverse?: boolean
+): void
+
+/**
+ * Visit children of tree which pass one of the tests
+ *
+ * @param tree abstract syntax tree to visit
+ * @param test test node
+ * @param visitor function to run for each node
+ * @param reverse visit the tree in reverse, defaults to false
+ * @typeParam T tree node
+ * @typeParam V node type to visit
+ */
+declare function visit<T extends Node>(
+  tree: T,
+  test: Array<Test<any>>,
+  visitor: Visitor<Node>,
+  reverse?: boolean
+): void
+
+/**
+ * Visit children of a tree
+ *
+ * @param tree abstract syntax tree to visit
+ * @param visitor function to run for each node
+ * @param reverse visit the tree in reverse, defaults to false
+ * @typeParam T tree node
+ */
+declare function visit<T extends Node>(
+  tree: T,
+  visitor: Visitor<Node>,
+  reverse?: boolean
+): void
+
+export = visit
---
+++ types/unist-util-visit-tests.ts
@@ -0,0 +1,161 @@
+import {Node, Parent} from 'unist'
+import unified = require('unified')
+import visit = require('unist-util-visit')
+
+/*=== setup ===*/
+const sampleTree = {
+  type: 'root',
+  children: [
+    {
+      type: 'heading',
+      depth: 1,
+      children: []
+    }
+  ]
+}
+
+interface Heading extends Parent {
+  type: 'heading'
+  depth: number
+  children: Node[]
+}
+
+interface Element extends Parent {
+  type: 'element'
+  tagName: string
+  properties: {
+    [key: string]: unknown
+  }
+  content: Node
+  children: Node[]
+}
+
+const headingTest = (node: Node): node is Heading => node.type === 'heading'
+const elementTest = (node: Node): node is Element => node.type === 'element'
+
+/*=== missing params ===*/
+// $ExpectError
+visit()
+// $ExpectError
+visit(sampleTree)
+
+/*=== visit without test ===*/
+visit(sampleTree, node => {})
+visit(sampleTree, (node: Node) => {})
+// $ExpectError
+visit(sampleTree, (node: Element) => {})
+// $ExpectError
+visit(sampleTree, (node: Heading) => {})
+
+/*=== visit with type test ===*/
+visit(sampleTree, 'heading', node => {})
+visit(sampleTree, 'heading', (node: Heading) => {})
+// $ExpectError
+visit(sampleTree, 'not-a-heading', (node: Heading) => {})
+// $ExpectError
+visit(sampleTree, 'element', (node: Heading) => {})
+
+visit(sampleTree, 'element', node => {})
+visit(sampleTree, 'element', (node: Element) => {})
+// $ExpectError
+visit(sampleTree, 'not-an-element', (node: Element) => {})
+// $ExpectError
+visit(sampleTree, 'heading', (node: Element) => {})
+
+/*=== visit with object test ===*/
+visit(sampleTree, {type: 'heading'}, node => {})
+visit(sampleTree, {random: 'property'}, node => {})
+
+visit(sampleTree, {type: 'heading'}, (node: Heading) => {})
+visit(sampleTree, {type: 'heading', depth: 2}, (node: Heading) => {})
+// $ExpectError
+visit(sampleTree, {type: 'element'}, (node: Heading) => {})
+// $ExpectError
+visit(sampleTree, {type: 'heading', depth: '2'}, (node: Heading) => {})
+
+visit(sampleTree, {type: 'element'}, (node: Element) => {})
+visit(sampleTree, {type: 'element', tagName: 'section'}, (node: Element) => {})
+// $ExpectError
+visit(sampleTree, {type: 'heading'}, (node: Element) => {})
+// $ExpectError
+visit(sampleTree, {type: 'element', tagName: true}, (node: Element) => {})
+
+/*=== visit with function test ===*/
+visit(sampleTree, headingTest, node => {})
+visit(sampleTree, headingTest, (node: Heading) => {})
+// $ExpectError
+visit(sampleTree, headingTest, (node: Element) => {})
+
+visit(sampleTree, elementTest, node => {})
+visit(sampleTree, elementTest, (node: Element) => {})
+// $ExpectError
+visit(sampleTree, elementTest, (node: Heading) => {})
+
+/*=== usage as unified plugin ===*/
+unified().use(() => sampleTree => {
+  // duplicates the above type tests but passes in the unified transformer input
+
+  /*=== missing params ===*/
+  // $ExpectError
+  visit()
+  // $ExpectError
+  visit(sampleTree)
+
+  /*=== visit without test ===*/
+  visit(sampleTree, node => {})
+  visit(sampleTree, (node: Node) => {})
+  // $ExpectError
+  visit(sampleTree, (node: Element) => {})
+  // $ExpectError
+  visit(sampleTree, (node: Heading) => {})
+
+  /*=== visit with type test ===*/
+  visit(sampleTree, 'heading', node => {})
+  visit(sampleTree, 'heading', (node: Heading) => {})
+  // $ExpectError
+  visit(sampleTree, 'not-a-heading', (node: Heading) => {})
+  // $ExpectError
+  visit(sampleTree, 'element', (node: Heading) => {})
+
+  visit(sampleTree, 'element', node => {})
+  visit(sampleTree, 'element', (node: Element) => {})
+  // $ExpectError
+  visit(sampleTree, 'not-an-element', (node: Element) => {})
+  // $ExpectError
+  visit(sampleTree, 'heading', (node: Element) => {})
+
+  /*=== visit with object test ===*/
+  visit(sampleTree, {type: 'heading'}, node => {})
+  visit(sampleTree, {random: 'property'}, node => {})
+
+  visit(sampleTree, {type: 'heading'}, (node: Heading) => {})
+  visit(sampleTree, {type: 'heading', depth: 2}, (node: Heading) => {})
+  // $ExpectError
+  visit(sampleTree, {type: 'element'}, (node: Heading) => {})
+  // $ExpectError
+  visit(sampleTree, {type: 'heading', depth: '2'}, (node: Heading) => {})
+
+  visit(sampleTree, {type: 'element'}, (node: Element) => {})
+  visit(
+    sampleTree,
+    {type: 'element', tagName: 'section'},
+    (node: Element) => {}
+  )
+  // $ExpectError
+  visit(sampleTree, {type: 'heading'}, (node: Element) => {})
+  // $ExpectError
+  visit(sampleTree, {type: 'element', tagName: true}, (node: Element) => {})
+
+  /*=== visit with function test ===*/
+  visit(sampleTree, headingTest, node => {})
+  visit(sampleTree, headingTest, (node: Heading) => {})
+  // $ExpectError
+  visit(sampleTree, headingTest, (node: Element) => {})
+
+  visit(sampleTree, elementTest, node => {})
+  visit(sampleTree, elementTest, (node: Element) => {})
+  // $ExpectError
+  visit(sampleTree, elementTest, (node: Heading) => {})
+
+  return sampleTree
+})
