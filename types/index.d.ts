7cbfbf0bde49fb160c1da686c42bc8fe04cbfabb
Christian Murphy <Christian.Murphy.42@gmail.com>
2019-07-25 20:38:14 -0700

    types: leverage unist-util-is helpers

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,47 +1,11 @@
 // TypeScript Version: 3.5
 
 import {Node, Parent} from 'unist'
+import {Test} from 'unist-util-is'
 
 interface Visitor<V extends Node> {
   (node: V, index: number, parent: Node): void
 }
-
-/**
- * Check that type property matches expectation for a node
- *
- * @typeParam T type of node that passes test
- */
-type TestType<T extends Node> = T['type']
-
-/**
- * Check that some attributes on a node are matched
- *
- * @typeParam T type of node that passes test
- */
-type TestObject<T extends Node> = Partial<T>
-
-/**
- * Check if a node passes a test
- *
- * @typeParam T type of node that passes test
- */
-interface TestFunction<T extends Node> {
-  /**
-   * Check if a node passes a test
-   *
-   * @param node node to check
-   * @param index index of node in parent
-   * @param parent parent of node
-   */
-  (node: Node, index?: number, parent?: Parent): node is T
-}
-
-/**
- * Union of all the types of tests
- *
- * @typeParam T type of node that passes test
- */
-type Test<T extends Node> = TestType<T> | TestObject<T> | TestFunction<T>
 
 /**
  * Visit children of tree which pass a test
@@ -55,25 +19,8 @@ type Test<T extends Node> = TestType<T> | TestObject<T> | TestFunction<T>
  */
 declare function visit<V extends Node>(
   tree: Node,
-  test: Test<V>,
+  test: Test<V> | Array<Test<any>>,
   visitor: Visitor<V>,
-  reverse?: boolean
-): void
-
-/**
- * Visit children of tree which pass one of the tests
- *
- * @param tree abstract syntax tree to visit
- * @param test test node
- * @param visitor function to run for each node
- * @param reverse visit the tree in reverse, defaults to false
- * @typeParam T tree node
- * @typeParam V node type to visit
- */
-declare function visit(
-  tree: Node,
-  test: Array<Test<any>>,
-  visitor: Visitor<Node>,
   reverse?: boolean
 ): void
 
@@ -83,7 +30,6 @@ declare function visit(
  * @param tree abstract syntax tree to visit
  * @param visitor function to run for each node
  * @param reverse visit the tree in reverse, defaults to false
- * @typeParam T tree node
  */
 declare function visit(
   tree: Node,
